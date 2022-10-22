37936273906c19cd8d7b0ea5ea93bc4075ee36ce
igapiat <igapiat@gmail.com>
2022-10-22 16:31:25 +0200

    witaj2.cpp

---
+++ witaj2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "witaj2.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string imie;
+
+	cout << "Podaj imie: ";
+	cin >> imie;
+	
+	cout << "Cześć " << imie << " " << "! Miło Cię widzieć!\n";
+
+  return 0;
+}
